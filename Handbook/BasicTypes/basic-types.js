1fda9ecbb754eae429bd9738fa550faed601d9ed
Soon <serdeemail@gmail.com>
2020-03-27 18:14:12 +0800

    新增基本数据类型.md

---
+++ Handbook/BasicTypes/basic-types.js
@@ -0,0 +1,5 @@
+"use strict";
+var list = [1, true, "free"];
+list[1] = 100;
+list[3] = 200;
+list[10] = 200;
---
+++ Handbook/BasicTypes/basic-types.ts
@@ -0,0 +1,5 @@
+let list: any[] = [1, true, "free"];
+
+list[1] = 100;
+list[3] = 200;
+list[10] = 200;
