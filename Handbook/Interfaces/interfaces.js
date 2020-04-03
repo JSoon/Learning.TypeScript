0a7b1bc4d8653a49931fc0291ac7ec057464ab98
Soon <serdeemail@gmail.com>
2020-04-03 17:48:39 +0800

    完成类类型章节

--- Handbook/Interfaces/interfaces.js
+++ Handbook/Interfaces/interfaces.js
@@ -1,4 +1,17 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 function createSquare(config) {
     var newSquare = {
         color: "white",
@@ -12,9 +25,10 @@ function createSquare(config) {
     }
     return newSquare;
 }
-var mySquare = createSquare({
-    color: 'red'
-});
+var squareOptions = {
+    colour: "red"
+};
+var mySquare = createSquare(squareOptions);
 var p1 = {
     x: 10,
     y: 20
@@ -24,3 +38,41 @@ var noChange = [1, 2, 3, 4, 5];
 // let copyNoChange: Array<number> = noChange.push(3)
 var canChange = noChange;
 canChange.push(6);
+var Animal = /** @class */ (function () {
+    function Animal(theName) {
+        this.name = theName;
+    }
+    return Animal;
+}());
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog(name, breed) {
+        var _this = _super.call(this, name) || this;
+        _this.breed = breed;
+        return _this;
+    }
+    return Dog;
+}(Animal));
+var myAnimal = new Animal('my animal');
+var myDog = new Dog('dog', 'husky');
+console.log(myDog);
+var dogs;
+dogs = [myAnimal, myDog];
+var myArray = ["Alice", "Bob"];
+myArray[2] = "Mallory"; // error!
+var Clock = /** @class */ (function () {
+    function Clock(h, m) {
+        this.h = h;
+        this.m = m;
+        this.nation = '中国制造';
+    }
+    Clock.prototype.tick = function () {
+        console.log('滴，答，滴，答', this.nation);
+    };
+    return Clock;
+}());
+function createClock(ctor, hour, minute) {
+    return new ctor(hour, minute);
+}
+var myClock = createClock(Clock, 12, 0);
+myClock.tick();
--- Handbook/Interfaces/interfaces.ts
+++ Handbook/Interfaces/interfaces.ts
@@ -38,7 +38,84 @@ let p1: Point = {
 };
 p1.x = 5; // error!
 
-let noChange: ReadonlyArray<number> = [1, 2, 3, 4, 5]
+let noChange: ReadonlyArray < number > = [1, 2, 3, 4, 5]
 // let copyNoChange: Array<number> = noChange.push(3)
 let canChange: number[] = noChange as number[]
 canChange.push(6)
+
+
+
+class Animal {
+  name: string;
+
+  constructor(theName: string) {
+    this.name = theName
+  }
+}
+class Dog extends Animal {
+  breed: string;
+
+  constructor(name: string, breed: string) {
+    super(name)
+    this.breed = breed
+  }
+}
+
+
+// Error: indexing with a numeric string might get you a completely separate type of Animal!
+interface NotOkay {
+  [x: number]: Animal;
+  [x: string]: Dog;
+}
+let myAnimal = new Animal('my animal')
+let myDog = new Dog('dog', 'husky')
+console.log(myDog);
+let dogs: NotOkay;
+dogs = [myAnimal, myDog]
+
+
+
+interface NumberDictionary {
+  [index: string]: number;
+  length: number; // ok, length is a number
+  name: string; // error, the type of 'name' is not a subtype of the indexer
+}
+
+interface ReadonlyStringArray {
+  readonly[index: number]: string;
+}
+let myArray: ReadonlyStringArray = ["Alice", "Bob"];
+myArray[2] = "Mallory"; // error!
+
+
+
+
+
+interface ClockConstructor {
+  new(hour: number, minute: number): ClockInterface
+}
+
+interface ClockInterface {
+  nation: string
+  tick(): void
+}
+
+class Clock implements ClockInterface {
+  constructor(
+    public h: number,
+    public m: number
+  ) {}
+
+  nation = '中国制造'
+
+  tick() {
+    console.log('滴，答，滴，答', this.nation)
+  }
+}
+
+function createClock(ctor: ClockConstructor, hour: number, minute: number): ClockInterface {
+  return new ctor(hour, minute)
+}
+
+const myClock = createClock(Clock, 12, 0)
+myClock.tick()
