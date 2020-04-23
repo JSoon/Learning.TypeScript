fb3ec56142053661017cefd09aa32b0e7939f2b3
Soon <serdeemail@gmail.com>
2020-04-23 18:13:47 +0800

    [函数] 完成this部分内容

--- Handbook/Functions/functions.js
+++ Handbook/Functions/functions.js
@@ -1,4 +1,19 @@
 "use strict";
-function f(firstName) {
-    // make sure `this` is unusable in this standalone function
-}
+var Handler = /** @class */ (function () {
+    function Handler() {
+        var _this = this;
+        this.onClickBad = function (e) {
+            // oops, used `this` here. using this callback would crash at runtime
+            _this.info = e.type;
+            console.log(e.type);
+        };
+    }
+    return Handler;
+}());
+var h = new Handler();
+var uiElement = {
+    addClickListener: function (cb) {
+        document.body.onclick = cb;
+    }
+};
+uiElement.addClickListener(h.onClickBad); // error!
--- Handbook/Functions/functions.ts
+++ Handbook/Functions/functions.ts
@@ -1,3 +1,20 @@
-function f(this: void, firstName: string) {
-  // make sure `this` is unusable in this standalone function
+interface UIElement {
+  addClickListener(onclick: (this: void, e: Event) => void): void;
 }
+
+class Handler {
+  info!: string;
+  onClickBad = (e: Event) => {
+    // oops, used `this` here. using this callback would crash at runtime
+    this.info = e.type;
+    console.log(e.type);
+  }
+}
+
+let h = new Handler();
+let uiElement: UIElement = {
+  addClickListener: function(cb) {
+    document.body.onclick = cb
+  }
+}
+uiElement.addClickListener(h.onClickBad); // error!
