3bc922ae648218bc7faef18ddea2d1f9f383c4d3
Soon <serdeemail@gmail.com>
2020-04-23 22:21:30 +0800

    1. 完成函数章节 2. 新增文字类型章节

--- Handbook/Functions/functions.ts
+++ Handbook/Functions/functions.ts
@@ -18,3 +18,32 @@ let uiElement: UIElement = {
   }
 }
 uiElement.addClickListener(h.onClickBad); // error!
+
+let suits = ["hearts", "spades", "clubs", "diamonds"];
+
+function pickCard(x: any): any {
+  // Check to see if we're working with an object/array
+  // if so, they gave us the deck and we'll pick the card
+  if (typeof x == "object") {
+    let pickedCard = Math.floor(Math.random() * x.length);
+    // 返回卡牌的序号
+    return pickedCard;
+  }
+  // Otherwise just let them pick the card
+  else if (typeof x == "number") {
+    let pickedSuit = Math.floor(x / 13);
+    // 返回卡牌
+    return { suit: suits[pickedSuit], card: x % 13 };
+  }
+}
+
+let myDeck = [
+  { suit: "diamonds", card: 2 },
+  { suit: "spades", card: 10 },
+  { suit: "hearts", card: 4 }
+];
+let pickedCard1 = myDeck[pickCard(myDeck)];
+console.log("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+
+let pickedCard2 = pickCard(15);
+console.log("card: " + pickedCard2.card + " of " + pickedCard2.suit);
